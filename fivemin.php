57081d5005ecbd7e8bd1e5f3388b9dc488773b17
DPway <den.meteo.is@gmail.com>
2016-11-17 14:10:40 +0200

    version0.5

--- fivemin.php
+++ fivemin.php
@@ -3,44 +3,65 @@
 <head>
 	<title>Sat24 by METEO-IS</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="author" content="Denіs Pishniak" />
-	<meta name="description" content="Meteo weather satellite animation" />
-	<link rel="shortcut icon" type="image/x-icon" href="images/ico1.ico">
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<meta http-equiv="Cache-Control" content="no-store" />
+	<meta name="author" content="METEO-IS." />
+	<meta name="description" content="Meteo weather satellite animation with 5 min frame interval" />
+	<link rel="shortcut icon" type="image/x-icon" href="siteimg/ico1.ico">
+
+	<meta property="og:title" content="Sat24 by METEO-IS"/>
+	<meta property="og:site_name" content="Sat24 by METEO-IS"/>
+	<meta property="og:url" content="http://sat.meteo-is.in.ua/"/>
+	<meta property="og:description" content="Actual weather animation"/>
+	<meta property="og:image" content="http://sat.meteo-is.in.ua/siteimg/2016-11-03-06-00.jpg"/>
+
+	<link href="https://fonts.googleapis.com/css?family=Cairo:700" rel="stylesheet">	
+	<link rel="stylesheet" href="bootstr/bootstrap.min.css">
+  	<script src="bootstr/jquery-3.1.0.min.js"></script>
+  	<script src="bootstr/bootstrap.min.js"></script>
 
 
 <?php 
 for ($qf = 0; $qf < 36; $qf++) {
-    	$timearray = getdate(time() -10800 + 300*$qf - 10800);
-    	$roundMin = round($timearray[minutes] / 5) * 5;
-    	$namearray[$qf] = ('http://en.sat24.com/image?type=infraPolair&region=eu&timestamp=' . $timearray[year] .
-	sprintf("%02d",$timearray[mon]) . sprintf("%02d", $timearray[mday]) . sprintf("%02d", $timearray[hours]) . 
-	sprintf("%02d", $roundMin));
+    $timearray = getdate(time() -10800 + 300*$qf - 10800);
+    $roundMin = round($timearray[minutes] / 5) * 5;
+    $urlarray[$qf] = ('http://en.sat24.com/image?type=infraPolair&region=eu&timestamp=' . $timearray[year] .
+		sprintf("%02d",$timearray[mon]) . sprintf("%02d", $timearray[mday]) . sprintf("%02d", $timearray[hours]) . 
+		sprintf("%02d", $roundMin));
+     $namearray[$qf] = ($timearray[year] .'-'. sprintf("%02d",$timearray[mon]) .'-'. 
+     	sprintf("%02d", $timearray[mday]) .'-'. sprintf("%02d", $timearray[hours]) .'-'. 
+		sprintf("%02d", $roundMin) . '.jpg');
 	}
 
-for ($qf = 0; $qf < count($namearray); $qf++) {
-	echo('<link rel="prefetch" href="' . $namearray[$qf] . '">
+for ($qf = 0; $qf < count($urlarray); $qf++) {
+	echo('<link rel="prefetch" href="' . $urlarray[$qf] . '">
 	');
 }
 ?>
 
 	<style type="text/css">
 	 	body{text-align: center;}
+	 	p{font-size: 16px;}
 	 	.satframe{
 	 		position: absolute;
 	 		opacity: 0.1;}
 	 	#bars{position: absolute; 
 	 		width:95%;
-	 		padding-top: 5%;
+	 		padding-top: 3%;
 	 		padding-left: 2%;}
+	 	#bars>h3{color: #5F0;
+	 		text-shadow: 1px 1px 5px black;}
 	 	.progress-bar{background-color: #4D1;}
+		h1{font-family: 'Cairo', sans-serif;
+			padding-bottom: 2%;
+			color: #679;}
+		@media screen and (min-width: 240px) and (max-width: 700px) {
+			h1{font-size: 25px;}
+		}
 	</style>
 	<script type="text/javascript">
 		var lc = 0;
 		function loadcount(){
-			lc = lc + 100/ <?php echo(count($namearray)) ?> ;
+			lc = lc + 100/ <?php echo(count($urlarray)) ?> ;
 			document.getElementById("loadingbar").style.width = lc.toString() + "%"
 			document.getElementById("loadingbar").textContent = (Math.round(lc)).toString() + "%";
 		}
@@ -48,17 +69,18 @@ for ($qf = 0; $qf < count($namearray); $qf++) {
 </head>
 <body>
 <div class="container">
-	<h2> EUMETSAT images animated by METEO-IS.</h2>
+	<h1> EUMETSAT images animated by METEO-IS.</h1>
 	<div class="row">
   		<div class="col-sm-9">
 	
 <?php
-for ($qf = 0; $qf < count($namearray); $qf++) {
-	echo('<img class="satframe img-responsive" src="' . $namearray[$qf] . '" width="845" height="615" onload="loadcount()">
+for ($qf = 0; $qf < count($urlarray); $qf++) {
+	echo('<img class="satframe img-responsive" src="' . $urlarray[$qf] . '" width="845" height="615" onload="loadcount()">
 	');
 }
 ?>
 <div id="bars">
+	<h3>Loading...</h3>
 	<div class="progress" style="margin:1%;">
 		<div id="loadingbar" class="progress-bar progress-bar-striped active" role="progressbar"
 			  	aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%">
@@ -71,13 +93,17 @@ for ($qf = 0; $qf < count($namearray); $qf++) {
 	</div>
 </div>
 <?php
-echo('<img class="img-responsive" style="opacity: 0.0; padding-bottom: 30px;" src="' . $namearray[1] . '" width="845" height="615">');
+echo('<img class="img-responsive" style="opacity: 0.0; padding-bottom: 30px;" src="' . $urlarray[1] . '" width="845" height="615">');
 ?>	
 		</div>
 			<div class="col-sm-3">
+				<p>Frame interval</p>
 				<div class="btn-group-vertical">
+					<a id="bt2" href="1h.html" class="btn btn-default btn-lg btn-block" role="button">1 hour</a>
 					<a href="index.html" class="btn btn-default btn-lg btn-block" role="button">15 min</a>
 					<a href="#" class="btn btn-primary btn-lg btn-block" role="button">5 min</a>
+<!-- 					<a href="zipper5m.php" class="btn btn-success btn-lg btn-block" role="button" style="margin-top: 10px;">Download .zip</a>
+ -->					<!-- <button type="button" class="btn btn-success btn-lg btn-block" onclick="zipperjs()">Download .zip</button> -->
 				</div>
 			</div>
 		</div>
@@ -102,12 +128,12 @@ echo('<img class="img-responsive" style="opacity: 0.0; padding-bottom: 30px;" sr
 	var countim = 0;
 	function swichcont(){
 	  var satmap = document.getElementsByClassName("satframe");
-      if (countim <= <?php echo(count($namearray)-2); ?> ){
+      if (countim <= <?php echo(count($urlarray)-2); ?> ){
       	if (countim >= 1){
 				satmap[countim-1].style.opacity = 0;
       	}
       	else{
-				satmap[ <?php echo(count($namearray)-2); ?> ].style.opacity = 0;
+				satmap[ <?php echo(count($urlarray)-2); ?> ].style.opacity = 0;
       	}
 			satmap[countim].style.opacity = 1;
 			countim++;
@@ -117,12 +143,16 @@ echo('<img class="img-responsive" style="opacity: 0.0; padding-bottom: 30px;" sr
 	    	countim = 0;
 	    	timerId = setTimeout(swichcont, 2000);
 	    }
-
-	    
 	}
+
+
 
 	</script>
 
 </body>
 </html>
+
+
+
+
 
--- roboscript.php
+++ roboscript.php
@@ -1,58 +1,95 @@
 
 
 <?php 
-sleep(300); // Wait 5 min
+// Run pause | Отсрочка запуска
+sleep(300); // 300 - wait 5 min
 
+$arch15m_way = "satarchive/";
+$arch1h_way = "satarchive1h/";
+
+// Timestamp definition | Определен отметки времени
 $timearray = getdate(time() - 11100);             // -5 minutes -3 hours  (7500  -2 hours winter time)
 $roundMin = round($timearray[minutes] / 5) * 5;
 
+// Image download | Загрузка изображения 
+$imgfile = file_get_contents('http://en.sat24.com/image?type=infraPolair&region=eu&timestamp=' . $timearray[year] .
+	sprintf("%02d",$timearray[mon]) . sprintf("%02d", $timearray[mday]) . sprintf("%02d", $timearray[hours]) . 
+	sprintf("%02d", $roundMin));
+
+// Image saving | Сохранение изображения 
+file_put_contents(($arch15m_way . $timearray[year] . '-' . sprintf("%02d",$timearray[mon]) . '-' . 
+	sprintf("%02d", $timearray[mday]) . '-' . sprintf("%02d", $timearray[hours]) . '-' . 
+	sprintf("%02d", $roundMin) . '.jpg'), $imgfile);
+if ($roundMin == 0 || $roundMin == 60) {
+	file_put_contents(($arch1h_way . $timearray[year] . '-' . sprintf("%02d",$timearray[mon]) . '-' . 
+		sprintf("%02d", $timearray[mday]) . '-' . sprintf("%02d", $timearray[hours]) . '-' . 
+		sprintf("%02d", $roundMin) . '.jpg'), $imgfile);
+}
 // echo(('satarchive/' . $timearray[year] . '-' . sprintf("%02d",$timearray[mon]) . '-' . sprintf("%02d", $timearray[mday]) .
 // '-' . sprintf("%02d", $timearray[hours]) . '-' . sprintf("%02d", $roundMin) . '.jpg'));  
 //print_r(getdate());
-
-
-file_put_contents(('satarchive/' . $timearray[year] . '-' . sprintf("%02d",$timearray[mon]) . '-' . 
-	sprintf("%02d", $timearray[mday]) . '-' . sprintf("%02d", $timearray[hours]) . '-' . 
-	sprintf("%02d", $roundMin) . '.jpg'), 
-	file_get_contents('http://en.sat24.com/image?type=infraPolair&region=eu&timestamp=' . $timearray[year] .
-	sprintf("%02d",$timearray[mon]) . sprintf("%02d", $timearray[mday]) . sprintf("%02d", $timearray[hours]) . 
-	sprintf("%02d", $roundMin)));
-
 //file_get_contents('http://en.sat24.com/image?type=infraPolair&region=eu&timestamp=201609101610'));
 
-if ($handle = opendir('satarchive/')) {
-	$qf = 0;
-	$qdel = 0;
-    while (false !== ($filename = readdir($handle))) { 
-        if ($filename != "." && $filename != "..") { 
-            //echo "$filename\n";
-            $filenamea[$qf] = $filename;  		// создание масива имен файлов
-            $qf = $qf + 1;
-            if ($qf > 55){               		// предельное количество кадров
-            	$qdel = 5;
-            }
-            if (filesize('satarchive/' . $filename) < 1000){		// обработка пропущеного(не скачавшегося) кадра
-				file_put_contents(('satarchive/' . $filename), 
-					file_get_contents('http://en.sat24.com/image?type=infraPolair&region=eu&timestamp=' . $filename[0] . 
-						$filename[1] . $filename[2] . $filename[3] . $filename[5] . $filename[6] . $filename[8] . 
-						$filename[9] . $filename[11] . $filename[12] . $filename[14] . $filename[15]));
-            }
-        } 
-    }
-    closedir($handle); 
-    sort($filenamea);
-    for ($qf = 0; $qf < $qdel; $qf++) {
-		unlink('satarchive/' . $filenamea[$qf]);
+
+// Files in dirctory | файлы в директории
+function filename_reader($dirname) {
+	if ($handle = opendir($dirname)) {
+		$qf = 0;
+		while (false !== ($fname = readdir($handle))) { 
+			if ($fname != "." && $fname != "..") { 
+				$fnamearray[$qf] = $fname; 
+				$qf = $qf + 1;
+			}
+		}
 	}
-	writer($filenamea);
+	sort($fnamearray);
+	return quantity_control($dirname, $fnamearray);
 }
+
+// Files quantity control | Контроль количества файлов
+function quantity_control($dirname, $fnamearray) {
+	$qdel = 0;
+	if (count($fnamearray) > 55){			// предельное количество кадров анимации
+		$qdel = 5;}
+	for ($qf = 0; $qf < $qdel; $qf++) {
+		unlink($dirname . $fnamearray[$qf]);}
+	return(array_slice($fnamearray, $qdel, count($fnamearray)));   // возврат укороченного масива имен
+}
+
+// Image size control (not complited download) | Контроль размера изображения (Незавершонной загрузки)
+function quality_control($dirname, $fnamearray) {
+	$tqf = count($fnamearray);
+	for ($qf = $tqf; $qf < $tqf; $qf++) { 
+		$filename = $fnamearray[$qf];
+		if ($qf > ($tqf-10) && filesize($dirname . $fnamearray[$qf]) < 1000){
+			// for last 10 try to dawnload again | для 10 последних пытаемся загрузить снова
+			file_put_contents(($dirname . $filename), 
+				file_get_contents('http://en.sat24.com/image?type=infraPolair&region=eu&timestamp=' . $filename[0] . 
+					$filename[1] . $filename[2] . $filename[3] . $filename[5] . $filename[6] . $filename[8] . 
+					$filename[9] . $filename[11] . $filename[12] . $filename[14] . $filename[15]));
+		}
+		if ($qf < ($tqf-10) && filesize($dirname . $fnamearray[$qf]) < 1000){
+			// for other try to dawnload previous | для остальных пытаемся загрузить предыдущую 
+			$newmin = sprintf("%02d", (intval($filename[14] . $filename[15]) - 5));
+			file_put_contents(($dirname . $filename), 
+				file_get_contents('http://en.sat24.com/image?type=infraPolair&region=eu&timestamp=' . $filename[0] . 
+					$filename[1] . $filename[2] . $filename[3] . $filename[5] . $filename[6] . $filename[8] . 
+					$filename[9] . $filename[11] . $filename[12] . $newmin));
+		}
+	}
+}
+
+
+quality_control($dirname, filename_reader($arch15m_way));
+quality_control($dirname, filename_reader($arch1h_way));
+
 
 
 
 
 // Writer script 
-function writer($filearray) {
-$f = fopen("index.html", "w");  
+function writer($archdir, $filearray, $resultfilename) {
+$f = fopen($resultfilename, "w");  
 
 fwrite($f,'
 	<!DOCTYPE HTML>
@@ -60,30 +97,43 @@ fwrite($f,'
 <head>
 	<title>Sat24 by METEO-IS</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="author" content="Denіs Pishniak" />
-	<meta name="description" content="Meteo weather satellite animation" />
+	<meta http-equiv="Cache-Control" content="no-store" /> 
+	<meta name="author" content="METEO-IS." />
+	<meta name="description" content="Weather satellite animation." />
 	<link rel="shortcut icon" type="image/x-icon" href="images/ico1.ico">
 
+	<meta property="og:title" content="Sat24 by METEO-IS"/>
+	<meta property="og:site_name" content="Sat24 by METEO-IS"/>
+	<meta property="og:url" content="http://sat.meteo-is.in.ua/"/>
+	<meta property="og:description" content="Actual weather animation"/>
+	<meta property="og:image" content="http://sat.meteo-is.in.ua/siteimg/2016-11-03-06-00.jpg"/>
+
+	<link href="https://fonts.googleapis.com/css?family=Cairo:700" rel="stylesheet">	
 	<link rel="stylesheet" href="bootstr/bootstrap.min.css">
-  	<script src="bootstr/jquery-3.1.0.min.js"></script>
-  	<script src="bootstr/bootstrap.min.js"></script>
+	<script src="bootstr/jquery-3.1.0.min.js"></script>
+	<script src="bootstr/bootstrap.min.js"></script>
 	');
 
-	for ($qf = ($qdel+2); $qf < count($filearray); $qf++) {
-	fwrite($f, '<link rel="prefetch" href="/satarchive/' . $filearray[$qf] . '">
-	');
-	}
 fwrite($f,'
 	 <style type="text/css">
-	 	body{text-align: center;}
-	 	.satframe{
-	 		position: absolute;
-	 		opacity: 0.1;}
-	 	#bars{position: absolute; 
-	 		width:95%;
-	 		padding-top: 5%;
-	 		padding-left: 2%;}
-	 	.progress-bar{background-color: #4D1;}
+		body{text-align: center;}
+		p{font-size: 16px;}
+		.satframe{
+			position: absolute;
+			opacity: 0.1;}
+		#bars{position: absolute; 
+			width:95%;
+			padding-top: 3%;
+			padding-left: 2%;}
+		#bars>h3{color: #5F0;
+			text-shadow: 1px 1px 5px black;}
+		.progress-bar{background-color: #4D1;}
+		h1{font-family: "Cairo", sans-serif;
+			padding-bottom: 2%;
+			color: #679;}
+		@media screen and (min-width: 240px) and (max-width: 700px) {
+			h1{font-size: 25px;}
+		}
 	 </style>
 	 <script type="text/javascript">
 		var lc = 0;
@@ -96,43 +146,54 @@ fwrite($f,'
 </head>
 <body>
 	<div class="container"> 
-		<h2> EUMETSAT images animated by METEO-IS.</h2>
+		<h1> EUMETSAT images animated by METEO-IS.</h1>
 		<div class="row">
-  			<div class="col-sm-9" style="padding-left:0;">
+			<div class="col-md-9" style="padding-left:0;">
 	');
 
-for ($qf = ($qdel+1); $qf < count($filearray); $qf++) {
-	fwrite($f, '<img class="satframe img-responsive" src="/satarchive/' . $filearray[$qf] . '" width="845" height="615" onload="loadcount()">
+for ($qf = 0; $qf < count($filearray); $qf++) {
+	fwrite($f, '<img class="satframe img-responsive" src="/' . $archdir . $filearray[$qf] . '" width="845" height="615" onload="loadcount()">
 	');
 }
 
 fwrite($f,'	
 	<div id="bars">
+		<h3>Loading...</h3>
 		<div class="progress" style="margin:1%;">
 			<div id="loadingbar" class="progress-bar progress-bar-striped active" role="progressbar"
-				  	aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%">
+					aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%">
 			</div>
 		</div>
 		<div class="progress" style="margin:1%;">
 			<div id="timebar" class="progress-bar progress-bar-striped active" role="progressbar"
-				  	aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%">
+					aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%">
 			</div>
 		</div>
 	</div>
-	<img class="img-responsive" style="opacity: 0.0; padding-bottom: 30px;" src="/satarchive/' . $filearray[1] . '" width="845" height="615">
+	<img class="img-responsive" style="opacity: 0.0; padding-bottom: 30px;" src="/' . $archdir . $filearray[1] . '" width="845" height="615">
 
 			</div>
-			<div class="col-sm-3">
+			<div class="col-md-3">
+			<p>Frame interval</p>
 				<div class="btn-group-vertical">
-					<a href="#" class="btn btn-primary btn-lg btn-block" role="button">15 min</a>
-					<a href="fivemin.php" class="btn btn-default btn-lg btn-block" role="button">5 min</a>
-					<a href="zipper.php" class="btn btn-success btn-lg btn-block" role="button" style="margin-top: 10px;">Download</a>
+					<a id="bt2" href="1h.html" class="btn btn-default btn-lg btn-block" role="button">1 hour</a>
+					<a id="bt3" href="index.html" class="btn btn-primary btn-lg btn-block" role="button">15 min</a>
+					<a id="bt4" href="fivemin.php" class="btn btn-default btn-lg btn-block" role="button">5 min</a>
+					<a id="bt5" href="zipper15m.php" class="btn btn-success btn-lg btn-block" role="button" style="margin-top: 10px;">Download .zip</a>
 				</div>
 			</div>
 		</div>
 	</div>
 
 	<script type="text/javascript">
+	var pageUrl = window.location.href; 
+	if(pageUrl.indexOf("1h.html") + 1){
+		document.getElementById("bt3").classList.remove("btn-primary");
+		document.getElementById("bt3").classList.add("btn-default");
+	 	document.getElementById("bt2").classList.add("btn-primary");
+	 	document.getElementById("bt5").href = "zipper1h.php";
+	 	}
+
 	var tc = 0;
 	function run_timer(){
 		tc = tc + 1;
@@ -151,27 +212,27 @@ fwrite($f,'
 	var countim = 0;
 	function swichcont(){
 	  var satmap = document.getElementsByClassName("satframe");
-      if (countim <= '); 
+	  if (countim <= '); 
 fwrite($f, count($filearray)-2);
 fwrite($f,'){
-      	if (countim >= 1){
+		if (countim >= 1){
 				satmap[countim-1].style.opacity = 0;
-      	}
-      	else{
+		}
+		else{
 				satmap['); 
 fwrite($f, count($filearray)-2);
 fwrite($f,'].style.opacity = 0;
-      	}
+		}
 			satmap[countim].style.opacity = 1;
 			countim++;
 			timerId = setTimeout(swichcont, 100);
-	    }
-	    else {
-	    	countim = 0;
-	    	timerId = setTimeout(swichcont, 2000);
-	    }
+		}
+		else {
+			countim = 0;
+			timerId = setTimeout(swichcont, 2000);
+		}
 
-	    
+		
 	}
 
 	</script>
@@ -183,19 +244,27 @@ fwrite($f,'].style.opacity = 0;
 fclose($f); 
 } 
 
-function delitter($way_2){
-if ($handle = opendir($way_2)) {
-    while (false !== ($filename = readdir($handle))) {
-        if ($filename != "." && $filename != "..") { 
-            $fileTimeV = filemtime($way_2 . '/' . $filename);
-            if ($fileTimeV < time() - 3600){
-				unlink($way_2 . '/' . $filename);
-            }
-        }
-    }
+
+// Delete old reuested .zip files | удаляет старые запрошенные .zip файлы 
+function deletter($way_2){
+	if ($handle = opendir($way_2)) {
+		while (false !== ($filename = readdir($handle))) {
+			if ($filename != "." && $filename != "..") { 
+				$fileTimeV = filemtime($way_2 . '/' . $filename);
+				if ($fileTimeV < time() - 3600){   // oldest then 1hour | старее чем час
+					unlink($way_2 . '/' . $filename);
+				}
+			}
+		}
+	}
 }
-}
-delitter("zips15");
+
+writer($arch15m_way, filename_reader($arch15m_way), "index.html");
+if ($roundMin == 0 || $roundMin == 60) {
+	writer($arch1h_way, filename_reader($arch1h_way), "1h.html");}
+
+deletter("zips15");
+deletter("zips1h");
 
 ?>
 
